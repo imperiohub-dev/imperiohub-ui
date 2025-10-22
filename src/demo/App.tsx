e794171d58c37ae280e1e017c9b9dbce3971a682
RCYGM <jurgenmuller997@gmail.com>
2025-10-22 14:35:11 +0200

    create demo

---
+++ src/demo/App.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+//import { Button } from '../components/Button'; // importa alguno de tus componentes
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Sandbox ImperioHub UI</h1>
+    </div>
+  );
+}
